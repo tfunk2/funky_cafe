2cd0f2624a304b679d0006b138d5fe03967fd8a2
tfunk2 <tyler.j.funk2@gmail.com>
2020-08-05 16:07:20 -0600

    fetch meals and sides, set states

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    activePage: "meals",
+    meals: [],
+    sides: []
+  }
+
+  setActivePage = (currentPage) => {
+    this.setState({ activePage: currentPage })
+  }
+
+  componentDidMount() {
+    this.fetchMeals()
+  }
+
+  fetchMeals = () => {
+    fetch('http://localhost:3000/meals')
+      .then(response => response.json())
+      .then(allMeals => {
+        this.setState({ meals: allMeals })
+        console.log(allMeals)
+        console.log(this.state.meals)
+      })
+
+      fetch('http://localhost:3000/sides')
+      .then(response => response.json())
+      .then(allSides => {
+        this.setState({ sides: allSides })
+        console.log(allSides)
+        console.log(this.state.sides)
+      })
+  }
+
+  activePage = () => {
+    switch(this.state.activePage) {
+      case "meals":
+        return <h1>meals</h1>
+      case "sides":
+        return <h1>sides</h1>
+      case "ingredients":
+        return <h1>ingredients</h1>
+      default:
+        return <></> // Home page?
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1>Funky Cafe</h1>
+        </header>
+        {this.activePage()}
+      </div>
+    );
+  }
 }
 
 export default App;
